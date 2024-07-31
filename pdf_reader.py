6ae83a032f6e4bddf62cf3e7d74a1ad3a45e6ccd
_anuchkin_ <rubidium128@gmail.com>
2024-07-31 13:08:13 -0700

    Distributed everything into files

---
+++ pdf_reader.py
@@ -0,0 +1,184 @@
+from tts import *
+
+from correct_accent_words import *
+from numbers_into_words import *
+
+from pypdf import PdfReader
+
+
+
+class OpenPdf:
+	""" Class OpenPdf """
+
+	def __init__(self, path_to_book: str, page_number: int = 0,) -> None:
+		"""
+		Standard definition
+
+		Args:
+			page_number (int): The page from which the text will start to be voiced
+			path_to_book (str): Path to book
+		
+		Returns:
+			None
+		"""
+
+		self.reader = PdfReader(path_to_book)
+		self.number_of_pages = len(self.reader.pages)
+
+		self.path_to_book = path_to_book
+		self.page_number = page_number
+		self.last_page_number = self.number_of_pages - self.page_number
+		self.sentences_for_voice_acting = []
+
+	
+	def text_correction(self) -> None:
+		"""
+		A method for correcting text before voicing
+		"""
+		def remove_extra_symbols(page_text: str) -> str:
+			"""
+			A method for removing unnecessary characters
+
+			Args:
+				page_text (str): Text from page
+			
+			Returns:
+				str: Clean text
+			"""
+			for i in range(1001):
+				simbol = f"[{i}]"
+				page_text = page_text.replace(simbol, " ")
+			
+			page_text = page_text.replace("-", "")
+
+			page_text = page_text.replace("«", " ")
+			page_text = page_text.replace("»", " ")
+
+			page_text = page_text.replace("\xa0", " ")
+			
+			page_text = page_text.replace("  ", " ")
+
+			return page_text
+
+		def fixing_words_accent(page_text: str) -> str:
+			"""
+			The placement of accents in words from the dictionary "correct_words_dict"
+
+			Args:
+				page_text (str): Text from page
+			
+			Returns:
+				str: Text with correct accents
+			"""
+			for uncorrect, correct in correct_accent_words_dict.items():
+				page_text = page_text.replace(f" {uncorrect} ", correct)
+			
+			page_text = page_text.replace("  ", "")
+
+			return page_text
+
+		def replacing_numbers_with_text(page_text: str) -> str:
+			"""
+			A method for replacing different numbers with text
+
+			Args:
+				page_text (str): Text from page
+			
+			Returns:
+				str: Text with numbers
+			"""
+			for number, number_text in numbers_into_words_dict.items():
+				page_text = page_text.replace(number, number_text)
+			
+			page_text = page_text.replace("  ", "")
+
+			return page_text
+
+		def splitting_into_sentences(page_text: str) -> list:
+			"""
+			A method for splitting into three sentences
+
+			Args:
+				page_text (str): Text from page
+			
+			Returns:
+				list: The text is divided into three sentences in the list
+			"""
+			amount_sentences = 0
+			index_end_sentences = 0
+			sentences = ""
+
+			while True:
+				index_end_sentences = page_text.find(".", index_end_sentences)
+
+				if index_end_sentences != -1:
+					index_end_sentences += 1
+					amount_sentences += 1
+				else:
+					break
+				
+				if amount_sentences == 2:
+					sentences = page_text[:index_end_sentences+1]
+					page_text = page_text[index_end_sentences:]
+
+					self.sentences_for_voice_acting.append(sentences)
+					amount_sentences = 0
+			
+			return self.sentences_for_voice_acting
+
+		def fixing_punctuation(page_text: str) -> str:
+			"""
+			A method for correcting punctuation marks
+
+			Args:
+				page_text (str): Text from page
+			
+			Returns:
+				str: Text with corrected punctuation marks
+			"""
+			page_text = page_text.replace("(", ",")
+			page_text = page_text.replace(")", ",")
+			page_text = page_text.replace("—", ",")
+
+			return page_text
+
+
+		pg_num = self.page_number
+		lpg_num = self.last_page_number + 1
+
+		print(pg_num)
+		print(lpg_num)
+
+		for current_page_number in range(pg_num, lpg_num):
+			current_page = self.reader.pages[current_page_number]
+
+			page_text = current_page.extract_text()
+			page_text = page_text.lower()
+
+			page_text = remove_extra_symbols(page_text)
+			page_text = fixing_words_accent(page_text)
+			page_text = fixing_punctuation(page_text)
+			page_text = replacing_numbers_with_text(page_text)
+
+			sentences_for_voice_acting = splitting_into_sentences(page_text)
+
+		return sentences_for_voice_acting
+
+
+
+book = OpenPdf("books/Кибердзюцу.pdf", 19)
+
+sentences_for_voice_acting = book.text_correction()
+
+# print(sentences_for_voice_acting)
+# speak(sentences_for_voice_acting)
+
+count = 0
+# print(sentences_for_voice_acting[4:6])
+for sentence in sentences_for_voice_acting[7:]:
+	print(count)
+	speak(sentence)
+	count += 2
+
+
+
--- tts.py
+++ tts.py
@@ -1,7 +1,8 @@
-import torch
-import sounddevice as sd
 import time
-from pypdf import PdfReader
+import torch
+
+import sounddevice as sd
+
 
 language = 'ru'
 model_id = 'v4_ru'
@@ -9,166 +10,25 @@ speaker = 'xenia' # xenia, aidar, baya
 sample_rate = 48000
 device = torch.device('cpu')
 
-reader = PdfReader("Кибердзюцу.pdf")
 
-number_of_pages = len(reader.pages) # 223
-
-list_texts = []
-simple_text = ""
-number_page = 19
-
-correct_words_list = {
-	"вопервых": "воп+ервых",
-	"всё": "вс+ё",
-	"вовторых": "вовтор+ых",
-	"виновниками": "вин+овниками",
-	"политики": "пол+итики",
-	"безопасностью": "безоп+астностью",
-	"те": "т+е",
-	"или": "+или",
-	"опасностей": "оп+асностей",
-	"более": "б+олее",
-	"ниндзя": "н+индзя",
-	"хакерах": "х+акерах",
-	"королевы": "корол+евы",
-	"то": "т+о",
-	"найдете": "найд+ете",
-	"области": "+области",
-	"идей": "ид+ей",
-	"оригинальных": "оригин+альных",
-	"уже": "уж+е",
-	"другие": "друг+ие",
-	"организации": "организ+ации",
-	"организациях": "организ+ациях",
-	"средневековыми": "средневек+овыми",
-	"кибердзюцу": "кибердз+юцу",
-	"некоторые": "н+екоторые",
-	"использования": "исп+ользования",
-	"устранения": "устран+ения",
-	"уверен": "ув+ерен",
-	"реализации": "реализ+ации",
-	"работать": "раб+отать",
-	"некоторое": "н+екоторое",
-	"устоявшимся": "усто+явшимся",
-	"наемными": "на+емными",
-	"крестьян": "кресть+ян",
-	"самураев": "самур+аев",
-	"несколько": "н+есколько",
-	"конфликтов": "конфл+иктов",
-	"трактате": "тракт+ат+е",
-	"внимателен": "вним+ателен",
-	"информацию": "информ+ацию",
-	"постепенно": "постеп+енно",
-	"ученику": "ученик+у",
-	"этой": "+этой",
-	"некоторые": "н+екоторые",
-	"действительно": "действ+ительно",
-	"наверняка": "наверняк+а",
-	"устоявшимся": "усто+явшимся",
-	"поразному": "пор+азному",
-	"нанимали": "наним+али",
-	"методы": "м+етоды",
-	"таким": "так+им",
-	"противник": "прот+ивник",
-	"внимателен": "вним+ателен",
-	"наносить": "нанос+ить",
-	"смотрит": "см+отрит",
-	"агенты": "аг+енты",
-	"бреши": "бр+еши",
-	"совершения": "соверш+ения",
-	"саботажа": "сабот+ажа",
-	"поджога": "подж+ога",
-	"мастерство": "мастерств+о",
-	"методы": "м+етоды",
-	"он": "+он",
-	"да": "д+а",
-	"она": "он+а",
-	"те": "т+е",
-	"или": "+или",
-	"иные": "ин+ые",
-	"во": "в+о",
-	"время": "вр+емя",
-	"говорится": "говор+ится",
-	"продолжительной": "продолж+ительной",
-	"проникали": "проник+али",
-	"они": "",
-}
-
-for page in range(200):
-	page = reader.pages[number_page]
-	number_page += 1
-
-	example_text = page.extract_text()
-	# print(example_text)
-
-	for i in range(301):
-		simbol = f"[{i}]"
-		example_text = example_text.replace(simbol, " ")
-
-	example_text = example_text.replace("  ", "")
-
-	example_text = example_text.lower()
-
-
-	for uncorrect_word, correct_word in correct_words_list.items():
-		example_text = example_text.replace(uncorrect_word, correct_word)
-
-	count_point = 0
-	for simbol in example_text:
-		if simbol == ".":
-			simbol = "..."
-			count_point += 1
-		
-		if simbol == "(" or simbol == ")":
-			simbol = "..."
-		
-		if simbol == "—":
-			simbol = "..."
-		
-		if simbol == "-":
-			simbol = ""
-		
-		if simbol == "..." and count_point == 3:
-			# simple_text += ".."
-			list_texts.append(simple_text)
-			example_text = example_text.replace(simple_text, "")
-			simple_text = ""
-			count_point = 0
-		
-		simple_text += simbol
-
-
-
-print(len(list_texts))
-
-model, _ = torch.hub.load(repo_or_dir='snakers4/silero-models',
-										  model='silero_tts',
-										  language=language,
-										  speaker=model_id)
+model, _ = torch.hub.load(
+	repo_or_dir='snakers4/silero-models',
+	model='silero_tts',
+	language=language,
+	speaker=model_id
+)
 
 
 model.to(device)  # gpu or cpu
 
-
 def speak(text: str):
-	audio = model.apply_tts(text=text,
-						speaker=speaker,
-						sample_rate=sample_rate)
+	audio = model.apply_tts(
+		text=text,
+		speaker=speaker,
+		sample_rate=sample_rate
+	)
 	
-
 	sd.play(audio, sample_rate)
 	time.sleep((len
 	(audio) / sample_rate) + 0.5)
 	sd.stop()
-
-
-number_sentence = 63
-slice_list = int(number_sentence / 3)
-
-if number_sentence != 0:
-	list_texts = list_texts[slice_list:]
-
-for text in list_texts:
-	print(f"Номер предложения: {number_sentence}")
-	speak(text)
-	number_sentence += 3
